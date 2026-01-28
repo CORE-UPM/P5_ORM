e73138a2e05df5b62b7f5fc7681c78736fff674c
Santiago Pavón <santiago.pavon@upm.es>
2026-01-28 13:32:57 +0100

    Fix formatting of config_file path assignment

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const Sequelize = require('sequelize');
 
-const config_file = process.env.DATABASE_CONFIG_PATH || path.resolve(path.join(__dirname + '..', 'config', 'config.json'));
+const config_file = process.env.DATABASE_CONFIG_PATH || path.resolve(path.join(__dirname, '..', 'config', 'config.json'));
 const config = require(config_file);
 
 const sequelize =     // Rellene aqui ...
